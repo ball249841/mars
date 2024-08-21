a5a3439fa2f5405d9b23346df095f9a710acc366
ball249841 <157933581+ball249841@users.noreply.github.com>
2024-08-21 19:15:16 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -300,8 +300,8 @@ def handle_message(event):
                                 uri='https://www.ptt.cc/bbs/Finance/index.html'
                             ),
                             URIAction(
-                                label='youtube 程式教學分享頻道',
-                                uri='https://www.youtube.com/channel/UCPhn2rCqhu0HdktsFjixahA'
+                                label='嗨皮漫畫',
+                                uri='https://liff.line.me/2006134057-ABmv37Lb'
                             )
                         ]
                     )
